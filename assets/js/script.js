727c14cf580739602007f1290e8127a6f7ca82f4
Roll Hub <rollmtende@gmail.com>
2022-05-09 14:03:52 -0400

    created psuedo code and header for index.html

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+//psuedo code
+
+//timer starts at 120 seconds
+//when "start" button clicked, timer starts counting down
+//questions 1 starts
+//question incorrect timer subtracts 10s
+//when questions answered  move to the next question
+//when all questions are answered or timer hits 0 the game is over
+//once game is over, add initials and save score
+//clicking view highscores shows the stored highscores
+
+//create variables
+//create functions
